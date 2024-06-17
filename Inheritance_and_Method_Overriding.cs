65d740a7a41c0cd99937bfb0b3f32d3b1bca711b
AsheleyAudrey <aaamarh005@st.ug.edu.gh>
2024-06-17 12:40:03 +0000

    Add inheritance and method overriding

---
+++ Inheritance_and_Method_Overriding.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AnimalSounds
+{
+    // Base class
+    class Animal
+    {
+        // Virtual method
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class Dog
+    class Dog : Animal
+    {
+        // Overriding the MakeSound method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class Cat
+    class Cat : Animal
+    {
+        // Overriding the MakeSound method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of Animal
+            Animal genericAnimal = new Animal();
+            genericAnimal.MakeSound(); // Output: Some generic sound
+
+            // Create an instance of Dog
+            Dog dog = new Dog();
+            dog.MakeSound(); // Output: Bark
+
+            // Create an instance of Cat
+            Cat cat = new Cat();
+            cat.MakeSound(); // Output: Meow
+
+            // Pause the console
+            Console.ReadLine();
+        }
+    }
+}
